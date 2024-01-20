18ba104cd5f0db4baebee54b60c4c5fd9150d3c1
Ramon Chiara <ramonchiara@gmail.com>
2024-01-20 01:51:46 -0300

    Nascimento de meninos está funcionando.

--- ditador.py
+++ ditador.py
@@ -1,6 +1,13 @@
 class Familia:
+
+    def __init__(self):
+        self.meninos = 0
+
     def get_meninos(self):
-        return 0
+        return self.meninos
 
     def get_meninas(self):
         return 0
+
+    def nasceu_menino(self):
+        self.meninos += 1
--- test_ditador.py
+++ test_ditador.py
@@ -5,7 +5,14 @@ from ditador import Familia
 
 class DitadorTest(unittest.TestCase):
 
+    def setUp(self):
+        self.f = Familia()
+
     def test_nova_familia_nao_tem_filhos(self):
-        f = Familia()
-        self.assertEqual(0, f.get_meninos())
-        self.assertEqual(0, f.get_meninas())
+        self.assertEqual(0, self.f.get_meninos())
+        self.assertEqual(0, self.f.get_meninas())
+
+    def test_ao_nascer_menino_o_numero_de_meninos_deve_aumentar(self):
+        self.f.nasceu_menino()
+        self.assertEqual(1, self.f.get_meninos())
+        self.assertEqual(0, self.f.get_meninas())
