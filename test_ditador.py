ca4e7671783ff6cea880bb4be8bf9bb05d42bb80
Ramon Chiara <ramonchiara@gmail.com>
2024-02-03 03:24:05 -0300

    Ao nascer uma menina, a família continua podendo ter mais filhos.

--- test_ditador.py
+++ test_ditador.py
@@ -28,3 +28,7 @@ class DitadorTest(unittest.TestCase):
     def test_ao_nascer_menino_familia_nao_pode_mais_ter_filhos(self):
         self.f.nasceu_menino()
         self.assertEqual(False, self.f.pode_ter_filhos())
+
+    def test_ao_nascer_menina_familia_continua_podendo_ter_mais_filhos(self):
+        self.f.nasceu_menina()
+        self.assertEqual(True, self.f.pode_ter_filhos())
