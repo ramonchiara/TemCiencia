9199a655dcb35d3e23292b553be8b87635787529
Ramon Chiara <ramonchiara@gmail.com>
2024-02-03 03:16:11 -0300

    Atributos meninos e meninas não são públicos.

--- ditador.py
+++ ditador.py
@@ -1,22 +1,22 @@
 class Familia:
 
     def __init__(self):
-        self.meninos = 0
-        self.meninas = 0
+        self._meninos = 0
+        self._meninas = 0
         self._pode_ter_filhos = True
 
     def get_meninos(self):
-        return self.meninos
+        return self._meninos
 
     def get_meninas(self):
-        return self.meninas
+        return self._meninas
 
     def nasceu_menino(self):
-        self.meninos += 1
+        self._meninos += 1
         self._pode_ter_filhos = False
 
     def nasceu_menina(self):
-        self.meninas += 1
+        self._meninas += 1
 
     def pode_ter_filhos(self):
         return self._pode_ter_filhos
