46ed24106fc24df479162f9c999d18eb045f03e9
Ramon Chiara <ramonchiara@gmail.com>
2024-02-04 17:46:41 -0300

    Melhoria na forma de "calcular" o resultado do método "pode_ter_filhos()"

--- ditador.py
+++ ditador.py
@@ -3,7 +3,6 @@ class Familia:
     def __init__(self):
         self._meninos = 0
         self._meninas = 0
-        self._pode_ter_filhos = True
 
     def get_meninos(self):
         return self._meninos
@@ -13,10 +12,9 @@ class Familia:
 
     def nasceu_menino(self):
         self._meninos += 1
-        self._pode_ter_filhos = False
 
     def nasceu_menina(self):
         self._meninas += 1
 
     def pode_ter_filhos(self):
-        return self._pode_ter_filhos
+        return self._meninos < 1
